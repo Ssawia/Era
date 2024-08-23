6e058f93be2885788552eb8a0d20c6bfd21098c9
Aiwass <awaissaltdc@gmail.com>
2024-08-23 13:53:07 -0300

    Esqueci do meu mano helper

---
+++ helpers.py
@@ -0,0 +1,21 @@
+import classes.entitiesC as entities
+
+def show_info_chara(obj : entities.Character):
+    resistances = ""
+    elements = ""
+
+    for chave, valor in obj.attributes.resistances.items():
+        resistances += f"{chave}: {valor}({obj.attributes.resistancesBase[chave]})|"
+
+    for chave, valor in obj.attributes.elements.items():
+        elements += f"{chave}: {valor}|"
+    strg = f'''
+[{obj.name}]-->[Uuid: {obj.uuid}]
+|HP: {obj.attributes.status.hp}/{obj.attributes.status.maxHp}({obj.attributes.status.regenHp}) |SP: {obj.attributes.status.sp}/{obj.attributes.status.maxSp}({obj.attributes.status.regenSp}) |MP: {obj.attributes.status.mp}/{obj.attributes.status.maxMp}({obj.attributes.status.regenMp}) |SY: {obj.attributes.status.sanity}/{obj.attributes.status.maxSanity}
+|ATK: {obj.attributes.status.atk} |ATKM: {obj.attributes.status.atkM} |DEF: {obj.attributes.status.df} |DEFM: {obj.attributes.status.dfM} |RES: {obj.attributes.status.res} |RESM: {obj.attributes.status.resM} 
+|DGE: {obj.attributes.status.dodge} |SPD: {obj.attributes.status.spd} |CRIT: {obj.attributes.status.crit:.1f}% |CRITD: {obj.attributes.status.maxCrit:.1f}% |ITEM: {obj.attributes.status.item}% |LOAD: {obj.attributes.status.load}/kg
+
+[Elements]: {elements}
+[Resistances]: {resistances}
+'''
+    print(strg)
